6768c9514c22081ac5d738d76cb914cdd990c6e7
Van-molinari <vanessabmolinari@hotmail.com>
2023-09-26 07:59:02 -0300

    Updating
    Updating:
    - requirements.txt: Fixing lib names and adding new ones;
    - __init__.py: Implementing CORS in APIs to allow external hosts do do the requests.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint, render_template
 from flask_restx import Api
+from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.controllers import routes
 
 app = Flask(__name__)
+CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 blueprint = Blueprint('api', __name__)
 app.register_blueprint(blueprint)
