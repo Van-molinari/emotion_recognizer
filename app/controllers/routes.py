72ae249bef1ca86f99c5f919bf571b2461b1e79b
Van-molinari <vanessabmolinari@hotmail.com>
2023-10-04 23:13:20 -0300

    API x DB integration with Docker
    Environment virtualization with Docker:
    - DB in a Docker container;
    - API using MySQL tables for uploading audios;
    
    PENDING:
    - Rebase APIs for search values into database;
    - Create new API method for searching latest tickets data (predictions);
    - Insert API limits.

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -3,6 +3,7 @@ from flask_restx import Resource, Namespace
 from concurrent.futures import ThreadPoolExecutor
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
+from database import database
 import recognize
 import uuid
 
@@ -21,10 +22,9 @@ class UploadController(Resource):
         f = request.files['file']
         if f.filename.endswith(".wav") or f.filename.endswith(".mp3"):
             file_id = str(uuid.uuid1())
-            with open("media/files.csv", "+a") as file:
-                line = "\n" + file_id + "," + f.filename
-                file.write(line)
-            f.save("media/" + secure_filename(f.filename))
+            f.save("uploads/media/" + f.filename)
+            db = database.Database()
+            db.insert("audios", [file_id, f.filename])
             with ThreadPoolExecutor() as executor:
                 future = executor.submit(RecognizeController.get(self, file_id))
 
@@ -32,7 +32,7 @@ class UploadController(Resource):
         else:
             return {"error": "Not supported. Please, upload only WAV or MP3 files."}, 400
 
-api_search = Namespace('Search', description='Recognize human emotion and transcript audio speech')
+api_search = Namespace('Search', description='Search for uploaded scripts in database')
 
 search_model = api_search.parser()
 search_model.add_argument('id', location='id', type=str, required=True)
@@ -53,8 +53,23 @@ class SearchController(Resource):
                 final_list.append(data)
         return final_list, 200
 
-api_recognize = Namespace('Recognize', description='Recognize human emotion and transcript audio speech')
+@api_search.route('/<id>')
+class Search(Resource):        
+    @api_search.response(200, "OK")
+    @api_search.response(404, "Not Found")
+    def get(self):
+        with open("media/files.csv", "r") as file:
+            list_files = file.readlines()[1:]
+            final_list = []
+            for x in list_files:
+                data = {
+                    "id": x.split(",")[0].replace("\n", ""),
+                    "file": x.split(",")[1].replace("\n", "")
+                }
+                final_list.append(data)
+        return final_list, 200
 
+api_recognize = Namespace('Recognize', description='Recognize human emotion and transcript audio speech')
 @api_recognize.route('/<id>')
 class RecognizeController(Resource):        
     @api_recognize.response(200, "OK")
